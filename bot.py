e6248dda0f86365416d62166b3188a134f8ce167
Echo <36556830+EchoNoahGaming@users.noreply.github.com>
2019-08-10 12:12:23 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,9 @@ async def announcement(ctx, *, args):
 	embed.set_footer(text="By Echolandia Studios")
 	await ctx.send("", embed=embed)
 	
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 	
 bot.run(str(os.environ.get('BOT_TOKEN')))
