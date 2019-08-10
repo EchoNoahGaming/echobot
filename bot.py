8ca7eba467031e44e4ba7dca193f8444ef1a8cda
Echo <36556830+EchoNoahGaming@users.noreply.github.com>
2019-08-10 11:35:14 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,6 @@ bot = commands.Bot(command_prefix='-', description=description)
 @bot.event
 async def on_ready():
     print('Ready!')
-    game = discord.Game("with the API")
-    await client.change_presence(status=discord.Status.idle, activity=game)
 	
 @bot.command()
 async def announcement(ctx, *, args):
@@ -20,20 +18,4 @@ async def announcement(ctx, *, args):
 	embed.set_footer(text="By Echolandia Studios")
 	await ctx.send("", embed=embed)
 	
-@client.event
-async def on_message(message):
-    if message.content.startswith('$thumb'):
-        channel = message.channel
-        await channel.send('Send me that 👍 reaction, mate')
-
-        def check(reaction, user):
-            return user == message.author and str(reaction.emoji) == '👍'
-
-        try:
-            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-        except asyncio.TimeoutError:
-            await channel.send('👎')
-        else:
-            await channel.send('👍')
-
 bot.run(str(os.environ.get('BOT_TOKEN')))
